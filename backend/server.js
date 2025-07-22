f461283eab3bf8c12d45778b64cf0729a51fe310
BharadwajShivanya <144115901+BharadwajShivanya@users.noreply.github.com>
2025-07-23 00:52:06 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import nodemailer from 'nodemailer';
-import dotenv from 'dotenv';
 
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
